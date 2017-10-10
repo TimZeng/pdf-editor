f68fd3c488a6329c32603211a88f6177b36a9230
Tim Zeng <zengtiantian1122@gmail.com>
2017-10-09 18:44:45 -0700

    project basic setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+import PDFDisplay from './PDFDisplay';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div style={{backgroundColor:'grey'}}>
+        <center className='container'>
+          <h1>PDF Editor</h1>
+          <PDFDisplay />
+        </center>
+      </div>
     );
   }
 }
