c9a78c8e96ed3ac9a476c5fdca5b0fbec3caffd1
Tim Zeng <zengtiantian1122@gmail.com>
2017-10-12 16:15:25 -0700

    update and remove unused files and folders

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.app'));
+ReactDOM.render(<App />, document.querySelector('.app'));
